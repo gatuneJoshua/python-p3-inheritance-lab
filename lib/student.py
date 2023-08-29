5dfad326716d9f1199d1ef4e287d51ea3ee228be
Joshua <joshua.gatune@student.moringaschool.com>
2023-08-29 13:25:25 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,15 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        if knowledge is None:
+            self.knowledge = []
+        else:
+            self.knowledge = knowledge
+
+    def learn(self, lesson):
+        self.knowledge.append(lesson)
     
-    def learn(self):
-        pass
+   
